b3aab7c5c9bd7b07facb7620e7f2701d620a7a59
smallnest <smallnest@gmail.com>
2017-09-04 08:34:31 +0800

    buold condition

--- examples/goroutineid/goroutineid_go1.5_amd64.go
+++ examples/goroutineid/goroutineid_go1.5_amd64.go
@@ -1,7 +1,7 @@
-// 这个例子摘自 [goid](https://github.com/petermattis/goid), 演示了如果访问runtime内部的数据结构。
-
 // +build amd64 amd64p32
 // +build go1.5
+
+// 这个例子摘自 [goid](https://github.com/petermattis/goid), 演示了如果访问runtime内部的数据结构。
 
 package goid
 
